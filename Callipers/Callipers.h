180f93316986198b3801ddc2176ba1ff94a59d1c
Simon Cozens <simon@simon-cozens.org>
2015-12-19 19:04:36 +0900

    Initial Commit

---
+++ Callipers/Callipers.h
@@ -0,0 +1,18 @@
+//
+//  Callipers.h
+//  Callipers
+//
+//  Created by Simon Cozens on 19/12/2015.
+//  Copyright © 2015 Simon Cozens. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <GlyphsCore/GlyphsToolDrawProtocol.h>
+#import <GlyphsCore/GlyphsToolEventProtocol.h>
+#import <GlyphsCore/GlyphsPathPlugin.h>
+
+@interface Callipers : GlyphsPathPlugin {
+
+}
+
+@end
